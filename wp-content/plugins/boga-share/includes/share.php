da59a6506ca7eb814e8c4713097473e7e9dfdcb2
Yeray Díaz Rodríguez <yeray@mnopi.com>
2016-04-19 21:20:03 +0200

    bogashare estilo retoque desktop

--- wp-content/plugins/boga-share/includes/share.php
+++ wp-content/plugins/boga-share/includes/share.php
@@ -1,17 +1,14 @@
 <div id="compartir_opinion" class="text-center" data-postid="<?php echo get_the_ID(); ?>" data-appid="<?php echo sq_option('fb_app_id') ?>" data-logged="<?php echo is_user_logged_in();?>">
     <h3 id="compartir_opinion_header"></h3>
     <img id="bogashare_spinner" src="/wp-content/plugins/boga-share/assets/img/spinner2.gif" style="display: none; width: 80px">
-    <div id="compartir_opinion_desplegar" class="row">
-        <div class="col-md-6">
-            <a target="_blank" href="https://www.bogadia.com/sorteos/gana-kit-productos-crea-m-solo-darnos-opinion/"><img id="bogashare_img" class="img-responsive" src="/wp-content/plugins/boga-share/assets/img/concursocream.jpg"></a>
-        </div>
-        <div class="col-md-6">
+    <div id="compartir_opinion_desplegar">
+        <a target="_blank" href="https://www.bogadia.com/sorteos/gana-kit-productos-crea-m-solo-darnos-opinion/"><img id="bogashare_img" class="img-responsive" src="/wp-content/plugins/boga-share/assets/img/concursocream.jpg"></a>
+
 <!--            <p id="bogashare_description"><strong><a target="_blank" href="https://www.bogadia.com/sorteos/gana-kit-productos-crea-m-solo-darnos-opinion/">Más info</a></strong></p>-->
-            <form id="compartir_opinion_form">
+        <form id="compartir_opinion_form">
 <!--                <input id="share_msg" placeholder="¡Me parece increible!" style="margin: 0 auto;margin-bottom: 15px;border: 1px solid lightgray;width: 90%;height: 40px;">
 -->                <input id="share_submit" type="submit" value="Compartir en Facebook y participar" onclick=<?php if(!is_user_logged_in()){ ?>"myFacebookLogin()"<?php }else{  ?>"myFacebookLogin()"<?php } ?> ></input>
-            </form>
-        </div>
+        </form>
     </div>
     <button id="close_compartir_opinion"><em class="icon-angle-down" style="font-size: 25px;"></em></button>
 <!--    <button id="open_compartir_opinion" style="display: none;"><em class="icon-angle-up" style="font-size: 25px;"></em></button>

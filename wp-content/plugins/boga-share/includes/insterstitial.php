8ed514c01806330d7daaa95bb0229b35bc447b8e
Yeray Díaz Rodríguez <yeray@mnopi.com>
2016-05-19 12:36:58 +0200

    cambio de link mas info

--- wp-content/plugins/boga-share/includes/insterstitial.php
+++ wp-content/plugins/boga-share/includes/insterstitial.php
@@ -6,7 +6,7 @@
 		<img id="flores_img" src="/wp-content/plugins/boga-share/assets/img/flores.png">
 		<div id="compartir_opinion" data-postid="<?php echo get_the_ID(); ?>" data-appid="<?php echo sq_option('fb_app_id') ?>">
 			<button id="share_submit_insterstitial" class="share_submit" onclick="myFacebookLogin()"><i class="icon-facebook"></i> | Comparte este post para participar</button>
-			<a target="_blank" href="https://www.bogadia.com/sorteos/gana-kit-productos-crea-m-solo-darnos-opinion/"><p id="mas_info">Más info</p></a>
+			<a target="_blank" href="https://www.bogadia.com/sorteos/ganar-50-euros-compartiendo-post-facebook"><p id="mas_info">Más info</p></a>
 		</div>
 	</div>
 </div>

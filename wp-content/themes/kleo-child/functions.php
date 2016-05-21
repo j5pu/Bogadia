5367371a0b3f83330cd6c6b85fc10de1e24cb65b
Yeray Díaz Rodríguez <yeray@mnopi.com>
2016-05-21 17:08:35 +0200

    desactivar con

--- wp-content/themes/kleo-child/functions.php
+++ wp-content/themes/kleo-child/functions.php
@@ -526,14 +526,14 @@ function remove_cssjs_ver( $src ) {
 	if( strpos( $src, '?ver=' ) )
 		$src = remove_query_arg( 'ver', $src );
 	if (strpos($src, 'dynamic.css') == false && strpos($src, 'font') == false) {
-		global $counter;
+/*		global $counter;
 		$counter++;
 		if ($counter%2==0){
 				$src = str_replace("www.bogadia", "bogacdn.appspot", $src);
 		}else{
 			$src = str_replace("www.bogadia", "bogacdn2.appspot", $src);
 		}
-	}
+	}*/
 	return $src;
 }
 add_filter( 'style_loader_src', 'remove_cssjs_ver', 10, 2 );

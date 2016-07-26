c14b314e3d746d09c56caaa87df18f96a3f7519f
Yeray Díaz Rodríguez <yeray@mnopi.com>
2016-07-26 13:54:19 +0200

    verificar cuenta ok

--- wp-content/plugins/boga-contest/email_verify.php
+++ wp-content/plugins/boga-contest/email_verify.php
@@ -5,9 +5,22 @@ $id = $_GET['id'];
 
 $hash = get_user_meta($id, 'hash');
 
-if ($hash == $_GET['hash']){
+get_header();
+get_template_part( 'page-parts/general-title-section' );
+
+get_template_part( 'page-parts/general-before-wrap' );
+
+if ($hash[0] == $_GET['hash']){
     update_user_meta($id, 'verified', '1');
-    echo 'Cuenta verificada. ¡Muchas gracias!';
+    echo '<img class="img-responsive" style="margin:0 auto;" src="assets/img/sonrisa.png">';
+    echo '<h3 class="text-center">Cuenta verificada. Muchas gracias</h3>';
+
 }else{
-    echo '¡Que raro! No se ha podido verificar tu cuenta. Ponte en contacto enviando un mail a info@bogadia.com';
+    echo '<img class="img-responsive" style="margin:0 auto;" src="assets/img/sad.png">';
+    echo '<h3 class="text-center">¡Que raro! No se ha podido verificar tu cuenta. Ponte en contacto enviando un mail a info@bogadia.com</h3>';
+
 }
+posts_home();
+get_template_part('page-parts/general-after-wrap');
+get_footer();
+

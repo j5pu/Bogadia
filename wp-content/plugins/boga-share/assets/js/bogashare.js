ad36bbe1a311c06c644fa65af61c435648582102
Yeray Díaz Rodríguez <yeray@mnopi.com>
2016-08-11 14:31:36 +0200

    bogashare gracias por participar

--- wp-content/plugins/boga-share/assets/js/bogashare.js
+++ wp-content/plugins/boga-share/assets/js/bogashare.js
@@ -1,3 +1,26 @@
+function after_share(){
+    jQuery('#buttons_container').append('<h3 style="margin-top: 10px;">¡Genial! Ya estás participando. ¡Mucha suerte!</h3>');
+}
+
+function bind_share_buttons(){
+    jQuery('#whatsapp_share_button').on('click', function(){
+        ga('send', 'event', 'Bogashare2', 'click_whatsapp', 'click');
+        after_share()
+    });
+    jQuery('#facebook_share_button').on('click', function(){
+        ga('send', 'event', 'Bogashare2', 'click_facebook', 'click');
+        after_share()
+    });
+    jQuery('#twitter_share_button').on('click', function(){
+        ga('send', 'event', 'Bogashare2', 'click_twitter', 'click');
+        after_share()
+    });
+    jQuery('#pinterest_share_button').on('click', function(){
+        ga('send', 'event', 'Bogashare2', 'click_pinterest', 'click');
+        after_share()
+    });
+}
+
 jQuery(document).ready(function() {
     jQuery('#bogashare_banner').on('click', function(){
         ga('send', 'event', 'Bogashare2', 'click_banner', 'click');
@@ -5,24 +28,11 @@ jQuery(document).ready(function() {
         jQuery('#buttons_container').html(share_buttons);
         jQuery('#mobile_share_button').velocity('fadeOut');
         jQuery('#bogacontest_login_modal').modal({show:true});
-    });
-    jQuery('#whatsapp_share_button, #whatsapp_share_button_2').on('click', function(){
-        ga('send', 'event', 'Bogashare2', 'click_whatsapp', 'click');
-
-    });
-    jQuery('#facebook_share_button, #facebook_share_button_2').on('click', function(){
-        ga('send', 'event', 'Bogashare2', 'click_facebook', 'click');
-
-    });
-    jQuery('#twitter_share_button, #twitter_share_button_2').on('click', function(){
-        ga('send', 'event', 'Bogashare2', 'click_twitter', 'click');
-
-    });
-    jQuery('#pinterest_share_button, #pinterest_share_button_2').on('click', function(){
-        ga('send', 'event', 'Bogashare2', 'click_pinterest', 'click');
-
+        bind_share_buttons()
     });
     jQuery('#bogacontest_login_modal').on('hidden.bs.modal', function () {
         jQuery('#mobile_share_button').velocity('fadeIn');
+        bind_share_buttons()
     })
+    bind_share_buttons()
 });

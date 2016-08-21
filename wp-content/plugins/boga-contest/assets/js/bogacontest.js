44ec7d82577e4c26d88c7cf94dc32c3482dc1cf5
Yeray Díaz Rodríguez <yeray@mnopi.com>
2016-08-21 16:20:44 +0100

    bogacontest load more smooth effect bug fix

--- wp-content/plugins/boga-contest/assets/js/bogacontest.js
+++ wp-content/plugins/boga-contest/assets/js/bogacontest.js
@@ -158,7 +158,7 @@ var toolbar = {
                         $item.velocity('fadeIn');
                         setTimeout(function(){
                             grid.masonry('appended', $item);
-                        }, 250);
+                        }, 500);
 
                     }).done( function( instance ) {
                         toolbar.hide_show_load_more();

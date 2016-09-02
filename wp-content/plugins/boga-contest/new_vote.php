127e0aca24b892afbf454721884744fc5355041f
Yeray Díaz Rodríguez <yeray@mnopi.com>
2016-09-02 20:11:08 +0200

    bogacontest deactivate mail verify to vote

--- wp-content/plugins/boga-contest/new_vote.php
+++ wp-content/plugins/boga-contest/new_vote.php
@@ -3,13 +3,13 @@ require_once('../../../wp-load.php');
 $bogacontestant->setID($_POST['contestant_id']);
 $bogacontestant->setUserId($_POST['user_id']);
 
-$verified = get_user_meta($id, 'verified');
-$fb_id = get_user_meta($id, '_fbid');
+/*$verified = get_user_meta($id, 'verified');
+$fb_id = get_user_meta($id, '_fbid');*/
 
 
-if ($verified == 1 || !empty($fb_id)){
-    echo $bogacontestant->anotate_vote($_POST['voter_id']);
-}else{
+/*if ($verified == 1 || !empty($fb_id)){*/
+echo $bogacontestant->anotate_vote($_POST['voter_id']);
+/*}else{
     echo 'Revisa tu e-mail y verifica tu cuenta';
     include 'boga-contest.php';
     $hash = get_user_meta($id, 'hash');
@@ -19,4 +19,4 @@ if ($verified == 1 || !empty($fb_id)){
 }
 
 
-
+*/

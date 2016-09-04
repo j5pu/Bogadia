ce7b9a4ac8a6787a8673cde0c9c9acd60eca6080
Yeray Díaz Rodríguez <yeray@mnopi.com>
2016-09-04 14:13:03 +0200

    bogacontest free vote

--- wp-content/plugins/boga-contest/new_vote.php
+++ wp-content/plugins/boga-contest/new_vote.php
@@ -3,13 +3,13 @@ require_once('../../../wp-load.php');
 $bogacontestant->setID($_POST['contestant_id']);
 $bogacontestant->setUserId($_POST['user_id']);
 $voter_id = $_POST['voter_id'];
+echo $bogacontestant->anotate_vote($voter_id);
 
 $verified = get_user_meta($voter_id, 'verified');
 $fb_id = get_user_meta($voter_id, '_fbid');
 
 
 if ($verified[0] == '1' || !empty($fb_id)){
-    echo $bogacontestant->anotate_vote($voter_id);
 }else{
 
     $sended_mail = get_user_meta($voter_id, 'send_mail_verify');
@@ -17,12 +17,12 @@ if ($verified[0] == '1' || !empty($fb_id)){
     if ($sended_mail[0] = '0' || empty($sended_mail)){
         echo 'Te hemos enviado un mail. Revisa tu correo.';
 
-        $hash = get_user_meta($voter_id, 'hash');
         $info['id'] = $voter_id;
-        $info['hash'] = $hash[0];
         $user_info = get_userdata($voter_id);
         $info['user_email'] = $user_info->user_email;
         $info['first_name'] = $user_info->first_name;
+        $info['hash'] = $hash[0];
+        $hash = get_user_meta($voter_id, 'hash');
         bogacontest_mail_verify($info);
     }else{
         echo 'Revisa tu correo y verifica tu cuenta';

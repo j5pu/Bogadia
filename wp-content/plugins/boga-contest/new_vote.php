ea1556ec3ffac2a134b4d3386828cef49c882ff6
Yeray Díaz Rodríguez <yeray@mnopi.com>
2016-09-04 14:29:21 +0200

    bogacontest  email verify on vote hash bug

--- wp-content/plugins/boga-contest/new_vote.php
+++ wp-content/plugins/boga-contest/new_vote.php
@@ -15,16 +15,22 @@ if ($verified[0] == '1' || !empty($fb_id)){
     $sended_mail = get_user_meta($voter_id, 'send_mail_verify');
 
     if ($sended_mail[0] = '0' || empty($sended_mail)){
-        echo 'Te hemos enviado un mail. Revisa tu correo.';
+        echo '. Te hemos enviado un mail';
 
         $info['id'] = $voter_id;
         $user_info = get_userdata($voter_id);
         $info['user_email'] = $user_info->user_email;
         $info['first_name'] = $user_info->first_name;
-        $info['hash'] = $hash[0];
         $hash = get_user_meta($voter_id, 'hash');
+        if (empty($hash[0])){
+            $info['hash'] = md5( $user_info->user_nicename. microtime() );
+            add_user_meta( $info['id'], 'hash', $info['hash'] );
+            add_user_meta( $info['id'], 'verified', '0' );
+        }else{
+            $info['hash'] = $hash[0];
+        }
         bogacontest_mail_verify($info);
     }else{
-        echo 'Revisa tu correo y verifica tu cuenta';
+        echo '. Recuerda verificar tu cuenta';
     }
 }
